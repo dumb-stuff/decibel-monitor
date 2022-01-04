88274e97098cc5e92c4103bff92ce3cd1781a7ed
Rukchad Wongprayoon <mooping3roblox@gmail.com>
2022-01-04 13:30:43 +0700

    h

---
+++ cli.py
@@ -0,0 +1,11 @@
+import sounddevice as sd
+import numpy as np
+
+duration = -1  # seconds
+
+def print_sound(indata, outdata, frames, time, status):
+    volume_norm = np.linalg.norm(indata)*10
+    print(f"Volume is now {int(volume_norm)}db",end="\r")
+
+with sd.Stream(callback=print_sound):
+	sd.sleep(duration * 1000)
